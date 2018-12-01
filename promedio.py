bfc23fb4cf6c33d97b7d3b3e1f48e9fcb2323ea5
franmunoz <franshop.munoz@gmail.com>
2018-12-01 08:53:14 -0500

    añadiendo programapython

---
+++ promedio.py
@@ -0,0 +1,8 @@
+n = int(input('ingrese la cantidad de datos: '))
+suma = 0
+for i in range(n):
+    x = float(input('ingrese datos: '))
+    suma = suma + x
+    print (suma)
+prom = suma / n
+print ('prommedio es: ', prom)
